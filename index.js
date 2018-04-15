b5aab11e1693e95de4b4d9fcef808262bd6b7816
Muhammad Abdullah Zafar <mabdullahzafar08@gmail.com>
2018-04-15 10:18:04 +0500

    changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({hi: 'there'});
+	res.send({hello: "Ali"});
 });
 
 const PORT = process.env.PORT || 8000
