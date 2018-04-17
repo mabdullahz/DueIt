2b52d89cc1f3fdd311044fac3978c13ae9cab763
Muhammad Abdullah Zafar <mabdullahzafar08@gmail.com>
2018-04-18 01:48:19 +0500

    also commited dev keys file

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js, dont commit
+
+module.exports = {
+	googleClientID: '511452553967-uk3avrtrjvrs476gemi7fmg8qfd77tea.apps.googleusercontent.com',
+	googleClientSecret: '4KDcKMUMFY8cxp31RADP_HVU',
+	mongoURI: 'mongodb://dueit-team:XyloDueit107@ds239309.mlab.com:39309/dueit-dev',
+	cookieKey: 'ufebjdsnaalfhuanlsdfhuvdskjnascfasfasfmsa'
+};
