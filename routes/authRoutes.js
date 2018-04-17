4df50fcae48dbaf289ba4e2413c7dc942fbb9c32
Muhammad Abdullah Zafar <mabdullahzafar08@gmail.com>
2018-04-18 00:52:33 +0500

    testing again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,11 @@ const passport = require('passport');
 module.exports = (app) => {
 	
 	app.get(
-		'/',
-		console.log({Hello: ' World!'})
-	)
+		'/', (req, res) => {
+			res.send({Hello: "World"});
+		}
+	);
+	
 	app.get(
 		'/auth/google', 
 		passport.authenticate('google', {
